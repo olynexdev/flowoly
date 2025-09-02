251fb907b9709316814d9aeef0a1bb15d54947ba
olynexdev <teamolynexdev@gmail.com>
2025-09-02 15:52:02 +0600

    host on vercel

--- src/redux/baseQuery.ts
+++ src/redux/baseQuery.ts
@@ -2,7 +2,7 @@ import { fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 const baseUrl =
   process.env.NODE_ENV === "production"
-    ? "https://olynex.com/api"
+    ? "https://flowoly.com/api"
     : "http://localhost:3001/api";
 
 export const baseQuery = fetchBaseQuery({ baseUrl });
